881d9537bfa7e631d9b08a1975feb302781655bb
EC2 Default User <ec2-user@ip-172-31-4-225.eu-west-2.compute.internal>
2020-09-18 09:57:09 +0000

    Added aws metadata

--- testscript.sh
+++ testscript.sh
@@ -6,3 +6,8 @@ read -p "please enter the user name:" USERNAME
 echo $USERNAME
 read -s -p "please enter the password:" PASSWORD
 echo $PASSWORD
+echo $pwd
+PHNAME=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
+PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
+echo $PHNAME
+echo $PIP
