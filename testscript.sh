1be2f15217818b85db6d563a593189bc6511af78
EC2 Default User <ec2-user@ip-172-31-4-225.eu-west-2.compute.internal>
2020-09-18 10:02:33 +0000

    added echo commands

--- testscript.sh
+++ testscript.sh
@@ -9,5 +9,5 @@ echo $PASSWORD
 echo $pwd
 PHNAME=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
 PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
-echo $PHNAME
-echo $PIP
+echo "the public dns name is :"$PHNAME
+echo "the ec2 machine public ip is :" $PIP
